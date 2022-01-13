b61ebc978b2ad2f9e6cb7ad354d56919b3b8743d
OctoPussh <occtopush@gmail.com>
2022-01-12 20:18:26 -0500

    added plumbok command scripts

---
+++ bin/plumbok.php
@@ -0,0 +1,52 @@
+<?php
+declare(strict_types=1);
+
+namespace Plumbok;
+
+use Octopush\Plumbok\Command\CompileCommand;
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Input\InputInterface;
+
+foreach ([__DIR__ . '/../vendor/autoload.php', __DIR__ . '/../../../autoload.php'] as $file) {
+	if (file_exists($file)) {
+		require $file;
+		break;
+	}
+}
+
+/** @var Application $app */
+$app = new class('Plumbok', '{version}') extends Application {
+	/**
+	 * Gets the name of the command based on input.
+	 * @param InputInterface $input The input interface
+	 * @return string The command name
+	 */
+	protected function getCommandName(InputInterface $input): string {
+		return 'compile';
+	}
+
+	/**
+	 * Gets the default commands that should always be available.
+	 * @return array An array of default Command instances
+	 */
+	protected function getDefaultCommands(): array {
+		// Keep the core default commands to have the HelpCommand
+		// which is used when using the --help option
+		$defaultCommands = parent::getDefaultCommands();
+		$defaultCommands[] = new CompileCommand();
+
+		return $defaultCommands;
+	}
+
+	/**
+	 * Overridden so that the application doesn't expect the command
+	 * name to be the first argument.
+	 */
+	public function getDefinition() {
+		$inputDefinition = parent::getDefinition();
+		$inputDefinition->setArguments();
+
+		return $inputDefinition;
+	}
+};
+$app->run();
